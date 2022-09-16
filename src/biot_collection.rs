11d4fca44b5a9577bc766dd96f35df12ba418222
Robert Kottelin <robert.kottelin@gmail.com>
2022-09-16 16:08:27 +0200

    simplifying

--- src/biot_collection.rs
+++ src/biot_collection.rs
@@ -1,13 +1,65 @@
 use macroquad::prelude::*;
-use crate::biot::{Biot, TreePoint};
-use rstar::RTree;
-use std::collections::HashSet;
+/// Modulus operator to get toroidal world topology
+fn modulus<T>(a:T, b:T) -> T
+where T: std::ops::Rem<Output=T>+
+      std::ops::Add<Output = T>+
+      Copy
+{
+    ((a % b) + b) % b
+}
+/// A single biot
+#[derive(Clone, Debug)]
+pub struct Biot {
+    // Status
+    life: f32,
+    pub pos: Vec2,
+    speed: Vec2,
+}
+impl Biot {
+    /// Create a random biot
+    pub fn random_biot() -> Self {
+        let mut s = Self {
+            life: 0.,
+            pos: vec2(
+                rand::gen_range(0., 1.)*screen_width(),
+                rand::gen_range(0., 1.)*screen_height()
+                ),
+            speed: vec2(0., 0.),
+        };
+        // s.set_from_genome();
+        s.life = s.base_life();
+        s
+    }
+
+    pub fn step(&mut self) {
+        if self.life >= 1f32 {
+            BiotCollection::new(1);
+            self.life -= 200f32;
+        }
+        self.pos += self.speed;
+        self.pos.x = modulus(self.pos.x, screen_width());
+        self.pos.y = modulus(self.pos.y, screen_height());
+        self.speed *= 0.1;
+        self.random_move(0.2);
+    }
+
+    fn random_move(&mut self, speed: f32) {
+        self.accelerate(vec2(rand::gen_range(0., 1.)-0.5, rand::gen_range(0., 1.)-0.5).normalize(), speed);
+    }
+    /// Apply acceleration in a certain direction
+    fn accelerate(&mut self, dir:Vec2, speed: f32) {
+        self.speed += dir *speed;
+    }
+
+    fn base_life(&self) -> f32 {
+        1000f32
+    }
+}
 
 /// A collection of biots. Responsible for handling interactions between biots
 pub struct BiotCollection {
     biots: Vec<Biot>
 }
-
 impl BiotCollection {
     /// Create n random biots
     pub fn new(n: usize) -> Self {
@@ -20,63 +72,16 @@ impl BiotCollection {
     /// Compute one step of the simulation.
     pub fn step(&mut self) {
         let mut new : Vec<Biot> = Vec::new();
-        // R-star datastructure used for quickly locating neighbors
-        let tree : RTree<TreePoint> = RTree::bulk_load(
-            self.biots
-                .iter()
-                .enumerate()
-                .map(|(n,biot)|TreePoint {x:biot.pos.x as f64, y:biot.pos.y as f64, idx:n})
-                .collect());
-        // Move and reproduce biots
+
         for n in 0..(self.biots.len()) {
-            let mut feed_dir : Option<Vec2> = None;
-            if self.biots[n].intelligence > 0. {
-                for (other, d2) in tree.nearest_neighbor_iter_with_distance_2(&[self.biots[n].pos.x as f64, self.biots[n].pos.y as f64]) {
-                    if other.idx == n {
-                        // Filter the biot itself out of the query.
-                        continue;
-                    }
-                    if d2 as f32 > (self.biots[n].intelligence*self.biots[n].intelligence)*1600. {
-                        break;
-                    }
-                    if self.biots[n].stronger(&self.biots[other.idx]) {
-                        feed_dir = Some(vec2(other.x as f32 -self.biots[n].pos.x, other.y as f32 -self.biots[n].pos.y).normalize());
-                        break;
-                    }
-                }
-            }
-            let off = self.biots[n].step(&tree, feed_dir);
-            if let Some(offspring) = off {
-                new.push(offspring);
-            }
+            self.biots[n].step();
         }
-        // Compute biot interactions
-        let mut visited : HashSet<usize> = HashSet::new();
-        for f in tree.iter() {
-            visited.insert(f.idx);
-            for s in tree.locate_within_distance([f.x, f.y], 50.) //FIXME 30 is hardcoded
-            {
-                if ! visited.contains(&s.idx) { // Don't do it twice
-                    Biot::interact(&mut self.biots, f.idx, s.idx);
-                }
-            }
-        }
-        // Remove dead biots and add the new ones to the collection
-        self.biots.retain(|b| !b.dead());
         self.biots.append(&mut new);
     }
     /// Display the biot collection
     pub fn draw(&self) {
         for biot in self.biots.iter() {
-            if biot.intelligence>0. {
-                let size = 14.*(biot.photosynthesis+biot.attack+biot.defense+biot.motion);
-                draw_rectangle(biot.pos.x-size/2.,biot.pos.y-size/2., size, size, GREEN);
-            }
-            draw_circle(biot.pos.x,biot.pos.y, 7.*(biot.photosynthesis+biot.attack+biot.defense+biot.motion), GREEN);
-            draw_circle(biot.pos.x,biot.pos.y, 7.*(biot.attack+biot.defense+biot.motion), RED);
-            draw_circle(biot.pos.x,biot.pos.y, 7.*(biot.defense+biot.motion), DARKBLUE);
-            draw_circle(biot.pos.x,biot.pos.y, 7.*(biot.motion), BLUE);
-
+            draw_circle(biot.pos.x,biot.pos.y, 7., GREEN);
         }
     }
     /// The number of biots currently in our collection
--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,6 @@ mod biot;
 mod biot_collection;
 use biot_collection::BiotCollection;
 
-struct Textbox {
-    rect: Rect,
-}
-impl Textbox {
-    pub fn new() -> Self {
-        Self {
-            rect: Rect::new(
-                screen_width() * 0.0f32,
-                screen_height() * 0.0f32,
-                2000f32,
-                30f32,
-            ),
-        }
-    }
-    pub fn draw(&self) {
-        draw_rectangle(self.rect.x, self.rect.y, self.rect.w, self.rect.h, DARKGRAY);
-    }
-}
-
 fn window_conf() -> Conf {
     Conf {
         window_title: "Life Simulation".to_owned(),
@@ -30,27 +11,20 @@ fn window_conf() -> Conf {
         ..Default::default()
     }
 }
-
 #[macroquad::main(window_conf())]
 async fn main() {
-
-    rand::srand(miniquad::date::now().to_bits());
-    let mut biots = BiotCollection::new(600);
+    let mut biots = BiotCollection::new(60);
     let mut iterations = 1;
-    let textbox = Textbox::new();
-    // let plot = Plot::new([-4., -2., 1., 4.]);
-    // plot.show();
 
     loop {
         biots.step();
-        iterations += 1;
         clear_background(Color::new(0.30, 0.25, 0.16, 1.00));
         biots.draw();
-        textbox.draw();
         draw_text(&format!("FPS: {}, Cells: {}, Iterations: {}", get_fps(), biots.len(), iterations),
             screen_width()*0.01f32, screen_height()*0.02f32,
             22.,
             WHITE);
+        iterations += 1;
         next_frame().await
     }
 }
