005bef925d57246b750c98be928cc0865adc2bef
robertkottelin <robert.kottelin@gmail.com>
2023-12-23 17:32:27 +0100

    .

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 
+
 #[derive(Clone, Debug)]
 struct Bacteria {
-    name: String,
     age: u32,
     position: (i32, i32, i32),
     energy: f32,
@@ -9,9 +9,8 @@ struct Bacteria {
 }
 
 impl Bacteria {
-    fn new(name: String, age: u32, position: (i32, i32, i32), energy: f32, genome: Vec<u8>) -> Bacteria {
+    fn new(age: u32, position: (i32, i32, i32), energy: f32, genome: Vec<u8>) -> Bacteria {
         Bacteria {
-            name,
             age,
             position,
             energy,
@@ -20,21 +19,49 @@ impl Bacteria {
     }
 }
 
+#[derive(Debug)]
+struct Collection {
+    bacteria: Vec<Bacteria>,
+}
+
 fn main() {
-    let mut bacteria = Bacteria::new(
-        String::from("E. coli"),
-        0,
-        (0, 0, 0),
-        100.0,
-        vec![0, 1, 0, 1],
-    );
+    let mut collection = Collection {
+        bacteria: vec![
+            Bacteria::new(0, (0, 0, 0), 100.0, vec![0, 1, 0, 1]),
+            Bacteria::new(0, (1, 1, 1), 100.0, vec![1, 0, 1, 0]),
+            // More initial bacteria if needed
+        ],
+    };
+
+    let mut iteration = 0;
 
     loop {
-        bacteria.age += 1;
-        bacteria.energy -= 0.1;
+        let mut to_remove = vec![];
+        let mut new_bacteria = vec![];
+        iteration += 1;
+        print!("Iteration: {} ", iteration);
 
-        if bacteria.energy <= 0.0 {
-            break;
+        let collection_len = collection.bacteria.len();
+
+        for (index, b) in collection.bacteria.iter_mut().enumerate() {
+            b.age += 1;
+            b.energy -= 0.2;
+
+            if b.energy <= 0.0 {
+                to_remove.push(index);
+            } else if (b.age % 10 == 0) && (b.energy > 20.0) && (collection_len < 100000) {
+                let child = Bacteria::new(0, b.position, 50.0, b.genome.clone()); // New bacteria start with less energy
+                new_bacteria.push(child);
+            }
         }
+
+        collection.bacteria.append(&mut new_bacteria);
+
+        for index in to_remove.iter().rev() {
+            collection.bacteria.remove(*index);
+        }
+
+        println!("Bacteria: {}", collection.bacteria.len());
+
     }
 }
